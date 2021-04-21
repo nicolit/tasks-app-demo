13a7c5254d75399b7a2b8a7d4a2771660fd1033f
Yael Nicole <yaelisad@gmail.com>
2021-04-21 14:01:31 +0300

    made all keys imported from config

--- src/firebase.js
+++ src/firebase.js
@@ -4,10 +4,10 @@ import "firebase/database";
 
 export const fireBaseConfigs = {
   apiKey: config.API_KEY,
-  authDomain: "kanban-board-875ad.web.app",
-  databaseURL: "https://kanban-board-875ad-default-rtdb.firebaseio.com/",
-  projectId: "kanban-board-875ad",
-  storageBucket: "kanban-board-875ad-default-rtdb",
+  authDomain: config.DOMAIN,
+  databaseURL: config.DB_URL,
+  projectId: config.PROJECT_ID,
+  storageBucket: config.STORAGE_BUCKET,
   messagingSenderId: config.SENDER_ID,
   appId: config.APP_ID
 };
